d413f1c077cc8a0244acc2941a158405455dd7f0
Ярослав <yaroslawtkachenko@gmail.com>
2017-12-05 23:10:54 +0200

    Complete Tests

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,83 +1,72 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_list, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = current_user.tasks
     respond_to do |format|
-      format.html{}
-      format.json{render :json => {tasks: @tasks}, status: :ok}
+      format.html {  }
+      format.json { render json: @tasks, status: :ok}
     end
   end
 
   def new
+    @task = Task.new
+  end
 
+  def edit
   end
 
   def create
-    if current_user.tasks.create(task_create_params)
+    @task = current_user.tasks.new(task_params)
+    if @task.save
+      respond_to do |format|
+        format.html { redirect_to controller: :tasks }
+        format.json { render json: @task, status: :ok}
+      end
+    else
+      respond_to do |format|
+        format.html { render :new }
+        format.json { render json: @task, status: :unprocessable_entity}
+      end
+    end
+  end
+
+  def update
+    if @task.update(task_params)
+      respond_to do |format|
+        format.html { redirect_to controller: :tasks }
+        format.json { render json: @task, status: :ok}
+      end
+    else
+      respond_to do |format|
+        format.html { render :edit }
+        format.json { render json: @task, status: :unprocessable_entity}
+      end
+    end
+  end
+
+  def destroy
+    if @task && @task.destroy
       respond_to do |format|
         format.html { redirect_to controller: :tasks}
         format.json { render json: @task, status: :ok}
       end
     else
       respond_to do |format|
-        format.html { render 'tasks/new' }
+        format.html { redirect_to controller: :tasks}
         format.json { render json: @task, status: :unprocessable_entity}
       end
-     end
-  end
-
-  def edit
-   @task = Task.find_by(id: params[:id])
-   # redirect_to '/'
-   if @task
-     respond_to do |format|
-       format.html { redirect_to controller: :tasks}
-       format.json { render json: @task, status: :ok}
-     end
-   else
-     respond_to do |format|
-       format.html { render 'tasks/edit' }
-       format.json { render json: @task, status: :unprocessable_entity}
-     end
-   end
-  end
-
-  def update
-    task = Task.find_by(id: params[:id])
-    if task.update(task_update_params)
-      respond_to do |format|
-        format.html {redirect_to controller: :tasks}
-        format.json { render json: task, status: :ok}
-      end
-    else
-      respond_to do |format|
-        format.html { render  'tasks/edit' }
-        format.json { render json: task, status: :unprocessable_entity}
-      end
     end
   end
 
-  def delete
-    task = Task.find_by(id: params[:id])
-    if task && task.destroy
-      respond_to do |format|
-        format.html { redirect_to controller: :tasks}
-        format.json { render json: task, status: :ok}
-      end
-    else
-      respond_to do |format|
-        format.html { redirect_to controller: :tasks}
-        format.json { render json: task, status: :unprocessable_entity}
-      end
-    end
+  private
+
+  def set_list
+    @task = Task.find_by(id: params[:id])
   end
 
-  def task_create_params
-    params.require(:task).permit(:name,:description,:importance,:expiry,:done)
-  end
-
-  def task_update_params
-    params.require(:task).permit(:name,:description,:importance,:expiry,:done)
+  def task_params
+    params.require(:task).permit(:name, :description, :expiry, :importance, :is_done)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get '/:locale' => 'tasks#index'
   root 'tasks#index'
-  get 'tasks/new', to: 'tasks#new', as: 'new_task'
-  get 'tasks/edit', to: 'tasks#edit', as: 'edit_task'
-  post 'tasks/new', to: 'tasks#create', as: 'create_task'
-  post 'tasks/update/:id', to: 'tasks#update', as: 'update_task'
-  delete 'tasks/delete/:id', to: 'tasks#delete', as: 'delete_task'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  scope "(:locale)", locale: /en|ru/ do
+  get '/', to:'tasks#index'
     resources :tasks
-  end
 end
